17b998115fdf6a8d21f488c2e49b434131b35fd7
Matthew Caron <matt@mattcaron.net>
2019-10-31 20:44:25 -0400

    Fix up my environment.

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-export PATH=/opt/gcc-xtensa-esp32-elf-1.22.0-80-g6c4433a-5.2.0/bin:$PATH
-export IDF_PATH=$(pwd)/esp-idf
-export TARGET_DIR=target/xtensa-none-elf/release
-export LIBCLANG_PATH=$HOME/git/rust/xtensa/llvm_build/lib
- 
+export XARGO_RUST_SRC=${HOME}/workspace/esp32/rust/rust-xtensa/src
+export IDF_PATH=${HOME}/workspace/esp32/espressif/esp-idf
+export PATH=${HOME}/workspace/esp32/rust/llvm_build/bin:${HOME}/workspace/esp32/rust/rust_build/bin:${HOME}/.espressif/tools/xtensa-esp32-elf/esp32-2019r1-8.2.0/xtensa-esp32-elf/bin/:${PATH}
+export TARGET_DIR=target/xtensa-esp32-none-elf/release
+export LIBCLANG_PATH=$HOME/workspace/esp32/rust/llvm_build/lib
